2d5be30cf349b071ebcfc1e5cefad1619730635a
Mateo Ruiz <mateorm03@gmail,com>
2026-04-21 22:25:27 -0500

    fixed app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import './App.css'
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
